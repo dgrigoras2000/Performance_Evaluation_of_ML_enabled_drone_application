3ddad7085462ef658a2bc0f3f3844aca90153fa3
DimitrisGrigoras <63289481+DimitrisGrigoras@users.noreply.github.com>
2023-03-06 23:22:06 +0200

    Update cloud_main.py

--- Cloud/code/cloud_main.py
+++ Cloud/code/cloud_main.py
@@ -1,6 +1,10 @@
 class CloudMain:
+    # Define a static method called "road_check" that takes in an argument called "info"
     @staticmethod
     def road_check(info):
+        # Check if the "info" argument is a dictionary
         if type(info) is dict:
+            # Return True if "info" is a dictionary
             return True
+        # Return False if "info" is not a dictionary
         return False
