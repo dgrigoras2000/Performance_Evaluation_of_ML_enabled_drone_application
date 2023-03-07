1a305da0ff8b8bee372ba39b8ad3f00c981f1fdc
DimitrisGrigoras <63289481+DimitrisGrigoras@users.noreply.github.com>
2023-03-07 17:42:17 +0200

    Update cloud_main.py

--- Cloud/code/cloud_main.py
+++ Cloud/code/cloud_main.py
@@ -1,9 +1,15 @@
+import logging
+
+
 class CloudMain:
     # Define a static method called "road_check" that takes in an argument called "info"
     @staticmethod
     def road_check(info):
         # Check if the "info" argument is a dictionary
         if type(info) is dict:
+            # Print the dictionary from basestation
+            print(f"Dictionary from basestation with number of vehicles: \n{info}")
+            logging.error(f"Dictionary from basestation with number of vehicles: \n{info}")
             # Return True if "info" is a dictionary
             return True
         # Return False if "info" is not a dictionary
