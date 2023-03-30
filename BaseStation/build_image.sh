cc404ec3556d1e868af9a4fe932d722dfa54fa55
DimitrisGrigoras <63289481+DimitrisGrigoras@users.noreply.github.com>
2023-03-30 23:53:28 +0300

    Delete BaseStation directory

--- BaseStation/build_image.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker build -t basestation-exp:1.1 .
--- BaseStation/code/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-declare -px > /tmp/.env
-chmod 0644 /tmp/.env
-uvicorn basestation_server:app1 --reload --port 8000
